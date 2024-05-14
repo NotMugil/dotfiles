2b89ac2fea59e546178f0b983de20b0f062016c1
NotMugil <mugilsubramani@gmail.com>
2024-05-14 13:56:02 +0530

    added configs

---
+++ config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,9 @@
+return {
+	"numToStr/Comment.nvim",
+	lazy = true,
+	keys = {
+		{ "gc", mode = { "n", "v", "o" }, desc = "Comment Selection" },
+		{ "gb", mode = { "n", "v", "o" }, desc = "Comment Block" },
+	},
+	opts = function() end,
+}
