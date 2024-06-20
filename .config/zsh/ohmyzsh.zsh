715490d06e7e122741d9f71b27dfb43d50064a26
NotMugil <mugilsubramani@gmail.com>
2024-06-20 09:23:06 +0530

    🚚rename: .oh-my-zsh to ohmyzsh

--- .config/zsh/ohmyzsh.zsh
+++ .config/zsh/ohmyzsh.zsh
@@ -1,15 +1,25 @@
-# Set ZSH Directory
-export ZSH="$HOME/.oh-my-zsh"
+# Path to your Oh My Zsh installation.
+export ZSH="$ZDOTDIR/ohmyzsh"
 
-# Set ZSH Theme
 ZSH_THEME="robbyrussell"
- 
-# Source ZSH Syntax highlighting catppuccin mocha theme
-source ~/.config/zsh/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh
 
-# Plugins
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
-# Source OHMYZSH.SH
-source ~/.config/zsh/.oh-my-zsh/oh-my-zsh.sh
+source ~/.config/zsh/ohmyzsh/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
 
