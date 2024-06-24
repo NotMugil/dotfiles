c3811b3f4f531f61c67fd8627e5b80ed1a9720b1
NotMugil <mugilsubramani@gmail.com>
2024-06-24 15:19:32 +0530

    style: modify waybar to match four themes 1/2

--- .scripts/theme.sh
+++ .scripts/theme.sh
@@ -1,113 +1,123 @@
 #!/bin/bash
 
-# Define constants for themes
-THEME_1="theme1"
-THEME_2="theme2"
-THEME_3="theme3"
-THEME_4="theme4"
+ROFI_DIR="$HOME/.config/rofi"
+THEMES_DIR="/usr/share/themes"
+gtk4="$HOME/.config/gtk-4.0"
+xsettings="$HOME/.config/xsettingsd/xsettingsd.conf"
 
-# Define theme-specific settings (replace with actual themes and settings)
-THEME_1_GTK_THEME="Orchis-Light-Compact"
-THEME_1_COLOR_SCHEME="prefer-light"
-THEME_1_WALLPAPER="$HOME/.wallpapers/theme1.jpg"
-THEME_1_WAYBAR_THEME="theme1.css"
-THEME_1_KITTY_THEME="Catppuccin-Latte"
+# Function to apply Latte theme with selected color
+apply_latte_theme() {
+	local color="$1"
+	echo "Applying $color Latte theme"
 
-THEME_2_GTK_THEME="Another-Theme"
-THEME_2_COLOR_SCHEME="another-color-scheme"
-THEME_2_WALLPAPER="$HOME/.wallpapers/theme2.jpg"
-THEME_2_WAYBAR_THEME="theme2.css"
-THEME_2_KITTY_THEME="Another-Kitty-Theme"
+	# Commands to apply GTK Theme
+	gsettings set org.gnome.desktop.interface gtk-theme catppuccin-latte-$color-standard+default
+        ln -sf /usr/share/themes/catppuccin-latte-$color-standard+default/gtk-4.0 ~/.config
+	sed -i "s/Net\/ThemeName \".*\"/Net\/ThemeName \"catppuccin-latte-$color-standard+default\"/g" ~/.config/xsettingsd/xsettingsd.conf
+	pkill xsettingsd
+      	xsettingsd &
 
-THEME_3_GTK_THEME="Third-Theme"
-THEME_3_COLOR_SCHEME="third-color-scheme"
-THEME_3_WALLPAPER="$HOME/.wallpapers/theme3.jpg"
-THEME_3_WAYBAR_THEME="theme3.css"
-THEME_3_KITTY_THEME="Third-Kitty-Theme"
+	# Commands to apply waybar theme	
+	ln -sf ~/.config/waybar/colors/catppuccin-latte.css ~/.config/waybar/colors/current-theme.css
+	pkill waybar
+	waybar &
 
-THEME_4_GTK_THEME="Fourth-Theme"
-THEME_4_COLOR_SCHEME="fourth-color-scheme"
-THEME_4_WALLPAPER="$HOME/.wallpapers/theme4.jpg"
-THEME_4_WAYBAR_THEME="theme4.css"
-THEME_4_KITTY_THEME="Fourth-Kitty-Theme"
 
-# Function to apply GTK theme and color scheme
-apply_gtk_theme() {
-    local theme="$1"
-    local color_scheme="$2"
-
-    gsettings set org.gnome.desktop.interface gtk-theme "$theme"
-    gsettings set org.gnome.desktop.interface cursor-theme "$theme"
-    gsettings set org.gnome.desktop.wm.preferences theme "$theme"
-    gsettings set org.gnome.desktop.interface icon-theme "$theme"
-    gsettings set org.gnome.desktop.interface cursor-theme "$theme"
-    gsettings set org.gnome.desktop.interface icon-theme "$theme"
 }
 
-# Function to switch Waybar theme
-switch_waybar_theme() {
-    local theme="$1"
+# Function to apply Frappe theme with selected color
+apply_frappe_theme() {
+	local color="$1"
+	echo "Applying $color Frappe theme"
 
-    ln -sf "$WAYBAR_CONFIG_DIR/themes/$theme" "$WAYBAR_CONFIG_DIR/style.css"
-    ~/.scripts/refresh_waybar.sh
+	# Commands to apply GTK Theme
+	gsettings set org.gnome.desktop.interface gtk-theme catppuccin-frappe-$color-standard+default
+        ln -sf /usr/share/themes/catppuccin-frappe-$color-standard+default/gtk-4.0 ~/.config	
+	sed -i "s/Net\/ThemeName \".*\"/Net\/ThemeName \"catppuccin-frappe-$color-standard+default\"/g" ~/.config/xsettingsd/xsettingsd.conf
+	pkill xsettingsd
+      	xsettingsd &
+
+	# Commands to apply waybar theme
+	ln -sf ~/.config/waybar/colors/catppuccin-frappe.css ~/.config/waybar/colors/current-theme.css
+	pkill waybar
+	waybar &
+
 }
 
-# Function to apply wallpaper
-apply_wallpaper() {
-    local wallpaper="$1"
+# Functicaon to apply Macchiato theme with selected color
+apply_macchiato_theme() {
+	local color="$1"
+	echo "Applying $color Macchiato theme"
 
-    swww img "$wallpaper" --transition-type any
+	
+	# Commands to apply GTK Theme
+	gsettings set org.gnome.desktop.interface gtk-theme catppuccin-macchiato-$color-standard+default
+        ln -sf /usr/share/themes/catppuccin-macchiato-$color-standard+default/gtk-4.0 ~/.config
+	sed -i "s/Net\/ThemeName \".*\"/Net\/ThemeName \"catppuccin-macchiato-$color-standard+default\"/g" ~/.config/xsettingsd/xsettingsd.conf
+	pkill xsettingsd
+      	xsettingsd &
+
+	# Commands to apply waybar theme
+	ln -sf ~/.config/waybar/colors/catppuccin-macchiato.css ~/.config/waybar/colors/current-theme.css
+	pkill waybar
+	waybar &
 }
 
-# Function to switch theme based on Rofi selection
-switch_theme() {
-    local theme_number="$1"
+# Function to apply Mocha theme with selected color
+apply_mocha_theme() {
+	local color="$1"
+	echo "Applying $color Mocha theme"
 
-    case "$theme_number" in
-        1)
-            apply_gtk_theme "$THEME_1_GTK_THEME" "$THEME_1_COLOR_SCHEME"
-            switch_waybar_theme "$THEME_1_WAYBAR_THEME"
-            apply_wallpaper "$THEME_1_WALLPAPER"
-            kitten themes --reload-in=all "$THEME_1_KITTY_THEME"
-            ;;
-        2)
-            apply_gtk_theme "$THEME_2_GTK_THEME" "$THEME_2_COLOR_SCHEME"
-            switch_waybar_theme "$THEME_2_WAYBAR_THEME"
-            apply_wallpaper "$THEME_2_WALLPAPER"
-            kitten themes --reload-in=all "$THEME_2_KITTY_THEME"
-            ;;
-        3)
-            apply_gtk_theme "$THEME_3_GTK_THEME" "$THEME_3_COLOR_SCHEME"
-            switch_waybar_theme "$THEME_3_WAYBAR_THEME"
-            apply_wallpaper "$THEME_3_WALLPAPER"
-            kitten themes --reload-in=all "$THEME_3_KITTY_THEME"
-            ;;
-        4)
-            apply_gtk_theme "$THEME_4_GTK_THEME" "$THEME_4_COLOR_SCHEME"
-            switch_waybar_theme "$THEME_4_WAYBAR_THEME"
-            apply_wallpaper "$THEME_4_WALLPAPER"
-            kitten themes --reload-in=all "$THEME_4_KITTY_THEME"
-            ;;
-        *)
-            echo "Invalid theme number"
-            ;;
-    esac
+	
+	# Commands to apply GTK Theme
+	gsettings set org.gnome.desktop.interface gtk-theme catppuccin-mocha-$color-standard+default
+        ln -sf /usr/share/themes/catppuccin-mocha-$color-standard+default/gtk-4.0 ~/.config
+	sed -i "s/Net\/ThemeName \".*\"/Net\/ThemeName \"catppuccin-mocha-$color-standard+default\"/g" ~/.config/xsettingsd/xsettingsd.conf
+	pkill xsettingsd
+      	xsettingsd &
+
+	# Commands to apply waybar theme
+	ln -sf ~/.config/waybar/colors/catppuccin-mocha.css ~/.config/waybar/colors/current-theme.css
+	pkill waybar
+	waybar &
+
 }
 
-# Main logic: Prompt user to select a theme using Rofi
-selected_theme=$(echo -e "$THEME_1\n$THEME_2\n$THEME_3\n$THEME_4" | rofi -dmenu -p "Select a theme:")
+# Array of color options
+colors=("blue" "flamingo" "green" "lavender" "maroon" "mauve" "peach" "pink" "red" "rosewater" "sapphire" "sky" "teal" "yellow")
 
-# Check if a theme was selected and switch accordingly
-if [ -n "$selected_theme" ]; then
-    case "$selected_theme" in
-        "$THEME_1" | "$THEME_2" | "$THEME_3" | "$THEME_4")
-            switch_theme "$selected_theme"
-            ~/.scripts/combine_configs.sh "$selected_theme"  # Adjust as per your configuration merging script
-            ;;
-        *)
-            echo "Invalid theme selection"
-            ;;
-    esac
-else
-    echo "No theme selected"
+# Array of theme options
+themes=("latte" "frappe" "macchiato" "mocha")
+
+# Use Rofi to select theme
+selected_theme=$(printf '%s\n' "${themes[@]}" | rofi -dmenu -theme $ROFI_DIR/theme.rasi -p "Select a theme:")
+
+# Use Rofi to select color
+selected_color=$(printf '%s\n' "${colors[@]}" | rofi -dmenu -theme $ROFI_DIR/theme.rasi -p "Select a color for $selected_theme theme:")
+
+# If no color is selected, set default color to "blue"
+if [[ -z "$selected_color" ]]; then
+    selected_color="blue"
 fi
+
+# Determine which theme function to call based on user selection
+case $selected_theme in
+    "latte")
+        apply_latte_theme "$selected_color"
+        ;;
+    "frappe")
+        apply_frappe_theme "$selected_color"
+        ;;
+    "macchiato")
+        apply_macchiato_theme "$selected_color"
+        ;;
+    "mocha")
+        apply_mocha_theme "$selected_color"
+        ;;
+    *)
+        echo "Invalid selection or no theme chosen."
+        exit 1
+        ;;
+esac
+
+# Add more commands/functions as needed to apply the theme with selected color across different applications.
