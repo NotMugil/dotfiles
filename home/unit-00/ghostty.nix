1cdaa8124d1c63aec369d775f4c8e0700a13b770
NotMugil <notmugil01@gmail.com>
2025-04-18 21:22:01 +0530

    feat: add ghostty config

--- home/unit-00/ghostty.nix
+++ home/unit-00/ghostty.nix
@@ -8,9 +8,19 @@
     settings = {
       theme = "Everblush";
       font-size = 11;
+      window-padding-x = 10;
+      window-padding-y = 10;
       keybind = [
-        "ctrl+h=goto_split:left"
+        "ctrl+j=goto_split:left"
+        "ctrl+i=goto_split:up"
+	"ctrl+k=goto_split:down"
         "ctrl+l=goto_split:right"
+        "shift+ctrl+j=new_split:left"
+        "shift+ctrl+i=new_split:up"
+	"shift+ctrl+k=new_split:down"
+        "shift+ctrl+l=new_split:right"
+	"super+t=new_window"
+	"super+d=close_window"
      ];
     };
    };
--- modules/services/zsh.nix
+++ modules/services/zsh.nix
@@ -7,10 +7,32 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
-      ll = "eza --icons -lah --group-directories-first";
-      la = "eza --icons -a --group-directories-first";
-      ls = "eza --icons --group-directories-first";
-    };
+      # quality of life aliases
+      c = "clear";
+      x = "exit";
+      h = "history -10"; # show last 10 commands
+      hc = "history -c"; # clear history
+      hg = "history | grep"; # +command
+      cp = "cp -rv"; # recursive and verbose
+      rm = "rm -rIv"; # recursive, interactive, and verbose `rm`
+      mv = "mv -v"; # verbose
+      mkdir = "mkdir -p"; # recursive mkdir
+#      mkcd = "function _mkcd() { mkdir -p "$1" && cd "$1"; }; _mkcd";
 
+      # eza aliases
+      ll = "eza --icons -lah --group-directories-first"; # list permissions
+      la = "eza --icons -a --group-directories-first"; # list all dir and files including hidden
+      ls = "eza --icons --group-directories-first"; # list all (not hidden)
+
+      # git aliases
+      g = "git";
+      ga = "git add";
+      gaa = "git add --all";
+      gb = "git branch";
+      gc = "git commit --verbose";
+      gcam = "git commit --all --message";
+      gd = "git diff";
+      gp = "git push";
+    };
   };
 }
